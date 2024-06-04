ff8f6b0054230b9b60debad766116ae19c818a4a
Renan Silva <renansilvaytB@gmail.com>
2024-06-04 20:41:03 -0300

    fix initial template

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import './App.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { App } from './App.jsx'
+import './index.css'
 
-export function App() {
-  return (
-    <h1>Hello World</h1>
-  )
-}
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
