b24b2a8cdd654479120c22362d9a115f5f918520
Renan Silva <renansilvaytB@gmail.com>
2024-06-07 17:57:15 -0300

    fix initial template

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Main } from './pages/Main'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Main />
-  </React.StrictMode>,
-)
