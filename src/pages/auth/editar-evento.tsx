6c43a5c5a5926ddfb7879ed706bf00e3f39ea6d4
Renan Silva <renansilvaytB@gmail.com>
2024-06-24 19:45:52 -0300

    feat: definição de layouts

---
+++ src/pages/auth/editar-evento.tsx
@@ -0,0 +1,7 @@
+export function EditarEvento() {
+  return (
+    <div>
+      <h1>Editar</h1>
+    </div>
+  )
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,30 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import { Home } from "./pages/Home";
+import { AppLayout } from "./pages/_layouts/app";
+import { AuthLayout } from "./pages/_layouts/auth";
+
+import { Home } from "./pages/app/home";
+import { EditarEvento } from "./pages/auth/editar-evento";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      }
+    ]
+  },
+  {
+    path: "/",
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "/editar-evento",
+        element: <EditarEvento />
+      }
+    ]
   }
 ])
