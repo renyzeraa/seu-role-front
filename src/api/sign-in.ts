8d52bda766c4fa21ec1e75c4699df7d579c5a978
Renan Silva <renansilvaytB@gmail.com>
2024-07-03 20:25:47 -0300

    faet: api get sign in

---
+++ src/api/sign-in.ts
@@ -0,0 +1,10 @@
+import { api } from '@/lib/axios'
+
+export interface SignInBody {
+  email: string
+  senha: string
+}
+
+export async function signIn({ email, senha }: SignInBody) {
+  await api.post('/authenticate', { email, senha })
+}
