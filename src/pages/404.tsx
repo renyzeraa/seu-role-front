174cbd519e05ef957056da6e268a5d7262290f63
Renan Silva <renansilvaytB@gmail.com>
2024-07-03 20:24:24 -0300

    feat: futuros ajustes

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 
 export function NotFound() {
+  // @TODO melhorar estilos e colocar logo
   return (
     <div className="flex h-screen flex-col items-center justify-center gap-2">
       <h1 className="text-4xl font-bold">Página não encontrada !</h1>
